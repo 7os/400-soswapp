0069c5db77fd2d6ca18502951b39c251e6d410d4
Sir Louis <me@sirlouis.io>
2020-04-19 23:26:22 +0100

    bug fix

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $page_name = "400";
     <link rel="stylesheet" href="<?php echo \html_style("base.css"); ?>">
   </head>
   <body>
-    <?php \setup_page('400','base', true, PRJ_HEADER_HEIGHT); ?>
+    <?php \TymFrontiers\Helper\setup_page('400','base', true, PRJ_HEADER_HEIGHT); ?>
     <?php include PRJ_INC_HEADER; ?>
     <section id="main-content">
 
